45c222478d47e44c37ee9aa7c770884539f83e86
emperatrizm <emperatriz.668@gmail.com>
2023-11-21 20:40:59 -0600

    nav structure and hamburger menu

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+//import Home from '../pages/Home';
+//import Contact from '../pages/Contact';
+//import Services from '../pages/Services';
+import Nav from "../src/components/nav";
+const App = () => {
+  return (
+    <div className='bg-slate-900'>
+        <Nav/>
+    </div>
+  )
+}
+
+export default App
