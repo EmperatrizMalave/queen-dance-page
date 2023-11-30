350b147e49b740fdfae24e9dadf11f24075ff42f
emperatrizm <emperatriz.668@gmail.com>
2023-11-30 16:15:49 -0600

    styles nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-//import Home from '../pages/Home';
+import Home from './pages/Home';
 //import Contact from '../pages/Contact';
 //import Services from '../pages/Services';
-import Nav from "../src/components/nav";
+import Nav from '../src/components/nav';
 const App = () => {
   return (
     <div className='bg-white'>
         <Nav/>
+        <Home />
     </div>
   )
 }
