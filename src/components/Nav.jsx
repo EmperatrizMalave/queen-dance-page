8308511d70f13a3faf052a446c2691f4e87853a9
emperatrizm <emperatriz.668@gmail.com>
2023-12-06 12:30:47 -0600

    first styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -37,19 +37,18 @@ Contacto</li>
   return (
     <nav>
         <div className='flex items-center justify-between py-2 bg-sky-500 '>
-            <p className='ml-8 text-slate-200'>QUEEN DANCE</p>
         <div className='lg:flex md:flex lg: flex-1 justify-end font-normal hidden'>
         <div>
             <ul className='gap-8 mr-16 flex items-center'>
             <img src={zumba} id='img' className='w-auto h-16'/>
     <Link spy={true} smooth={true} to="Home">
-            <li className='hover:text-blue-500 hover:border-fuchsia-600 cursor-pointer text-slate-200' style={{ fontFamily:'Dhurjati', fontSize: '18px' }}>Inicio</li>
+            <li className='hover:text-[#494946] text-white-600 cursor-pointer text-slate-200' style={{ fontFamily:'Dhurjati', fontSize: '18px' }}>Inicio</li>
     </Link>
     <Link spy={true} smooth={true} to="Services">
-            <li className='hover:text-blue-500 cursor-pointer text-slate-200' style={{ fontFamily:'Dhurjati', fontSize: '18px' }}>Nuestros servicios</li>
+            <li className='hover:text-[#494946] cursor-pointer text-slate-200' style={{ fontFamily:'Dhurjati', fontSize: '18px' }}>Nuestros servicios</li>
         </Link>
         <Link spy={true} smooth={true} to="Contact">
-            <li className='hover:text-blue-500 cursor-pointer text-slate-200' style={{ fontFamily:'Dhurjati', fontSize: '18px' }}>Contacto</li>
+            <li className='hover:text-[#494946] cursor-pointer text-slate-200' style={{ fontFamily:'Dhurjati', fontSize: '18px' }}>Contacto</li>
         </Link>
             </ul>
         </div>
